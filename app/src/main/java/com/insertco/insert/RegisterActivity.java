97994cbe53a9905b6171ffbef5af26bbf19bad11
jh3er <renaelworkspaces@gmail.com>
2017-04-01 22:21:08 +0700

    Add View Pager , 4 Layout Branding activity

--- app/src/main/java/com/insertco/insert/RegisterActivity.java
+++ app/src/main/java/com/insertco/insert/RegisterActivity.java
@@ -1,9 +1,13 @@
 package com.insertco.insert;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -11,6 +15,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        TransparentNotifBar();
     }
     public void intentBackLogin(View view) {
         // Do something in response to button
@@ -20,8 +28,17 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void intentGetStart(View view){
 
-        Intent intent = new Intent(this, branding.class);
+        Intent intent = new Intent(this, WelcomeActivity.class);
         startActivity(intent);
+    }
+
+    public void TransparentNotifBar(){
+//        harus minimal dari API 21 -> Lolipop
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 
 }
