1e396ccf6abc3cdff795d15b433e6ca2504fe135
jh3er <renaelworkspaces@gmail.com>
2017-05-24 20:36:12 +0700

    Login Activity Validation

--- app/src/main/java/com/insertco/insert/LoginActivity.java
+++ app/src/main/java/com/insertco/insert/LoginActivity.java
@@ -2,8 +2,15 @@ package com.insertco.insert;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.StringDef;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
+
+import static android.R.attr.password;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,5 +25,27 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void intentLogin(View view) {
+        EditText username = (EditText) findViewById(R.id.editTextUsername);
+        EditText password = (EditText) findViewById(R.id.editTextPassword);
+        String user = username.getText().toString();
+        String pass = password.getText().toString();
+        if(user.equals("admin") && pass.equals("adminjr"))
+        {
+            Intent intent = new Intent(this, homeActivity.class);
+            startActivity(intent);
+        }
+
+        else
+        {
+            Snackbar.make(view, "Wrong Username or Password", Snackbar.LENGTH_LONG)
+                  .setAction("Action", null).show();
+        }
+
+    }
+
 
 }
+
+    // Validation for email and password
+
