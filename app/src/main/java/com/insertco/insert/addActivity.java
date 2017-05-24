e83d7cd938e21d2449a244426cded0baa1d3497b
underway113 <jeradam123@yahoo.com>
2017-05-24 22:12:09 +0700

    Edit addActivity (still no intent)

--- app/src/main/java/com/insertco/insert/addActivity.java
+++ app/src/main/java/com/insertco/insert/addActivity.java
@@ -1,13 +1,80 @@
 package com.insertco.insert;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class addActivity extends AppCompatActivity {
+
+    Spinner spinnerSize;
+    Spinner spinnerType;
+    Spinner spinnerGsm;
+
+    ArrayAdapter<CharSequence> adapterSize;
+    ArrayAdapter<CharSequence> adapterType;
+    ArrayAdapter<CharSequence> adapterGsm;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        spinnerSize = (Spinner)findViewById(R.id.spinnerPaperSize);
+        adapterSize = ArrayAdapter.createFromResource(this,R.array.Size_Array,android.R.layout.simple_spinner_item);
+        adapterSize.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSize.setAdapter(adapterSize);
+
+
+        spinnerType = (Spinner)findViewById(R.id.spinnerPaperType);
+        adapterType = ArrayAdapter.createFromResource(this,R.array.Type_Array,android.R.layout.simple_spinner_item);
+        adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerType.setAdapter(adapterType);
+
+        spinnerGsm = (Spinner)findViewById(R.id.spinnerGSM);
+        adapterGsm = ArrayAdapter.createFromResource(this,R.array.GSM_Array,android.R.layout.simple_spinner_item);
+        adapterGsm.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGsm.setAdapter(adapterGsm);
+
+        spinnerSize.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        spinnerType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        spinnerGsm.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
