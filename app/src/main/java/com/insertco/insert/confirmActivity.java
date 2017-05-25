b076151582cadec952079da8789f8229ccde5f49
underway113 <jeradam123@yahoo.com>
2017-05-25 20:11:53 +0700

    add verification

--- app/src/main/java/com/insertco/insert/confirmActivity.java
+++ app/src/main/java/com/insertco/insert/confirmActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,7 +41,7 @@ public class confirmActivity extends AppCompatActivity {
     int verifCode = rand.nextInt(10000 - 1000) + 1000;
     String verif = String.valueOf(verifCode);
     public void intentDone(View view) {
-        Intent intent = new Intent(confirmActivity.this, homeActivity.class);
+        final Intent intent = new Intent(confirmActivity.this, homeActivity.class);
         NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(confirmActivity.this)
                 .setSmallIcon(R.drawable.print)
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
@@ -49,7 +52,30 @@ public class confirmActivity extends AppCompatActivity {
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(confirmActivity.this);
         notificationManager.notify(1, notificationBuilder.build());
 
-        startActivity(intent);
-        Toast.makeText(getApplicationContext(), "Your Order have been Confirmed", Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(confirmActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_verification, null);
+        final EditText editTextVerif = (EditText) mView.findViewById(R.id.etVerif);
+        Button buttonVerif = (Button) mView.findViewById(R.id.btnVerif);
+
+
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+        buttonVerif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editTextVerif.getText().toString().isEmpty()){
+                    Toast.makeText(confirmActivity.this,"Please insert the Verification Code",Toast.LENGTH_SHORT).show();
+                }
+                else if(!editTextVerif.getText().toString().equals(verif)){
+                    Toast.makeText(confirmActivity.this,"Wrong Verification Code",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    dialog.dismiss();
+                    startActivity(intent);
+                    Toast.makeText(getApplicationContext(), "Your Order have been Confirmed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
