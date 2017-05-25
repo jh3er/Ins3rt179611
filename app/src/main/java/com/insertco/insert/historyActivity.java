c86a554f0f0d888228a2a6fc1c86cd632fd12bf4
underway113 <jeradam123@yahoo.com>
2017-05-26 00:28:44 +0700

    add icon document

--- app/src/main/java/com/insertco/insert/historyActivity.java
+++ app/src/main/java/com/insertco/insert/historyActivity.java
@@ -7,15 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * Created by Jeremy Adam on 21/05/2017.
- */
-
 public class historyActivity extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_history, container, false);
     }
+
 }
