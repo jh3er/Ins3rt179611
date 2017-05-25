d263515048c7550d1baf5fdec92e9c1e234b56f5
underway113 <jeradam123@yahoo.com>
2017-05-25 15:27:18 +0700

    add textview activity_confirm

--- app/src/main/java/com/insertco/insert/confirmActivity.java
+++ app/src/main/java/com/insertco/insert/confirmActivity.java
@@ -1,7 +1,10 @@
 package com.insertco.insert;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class confirmActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class confirmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
+    }
+    public void intentDone(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(confirmActivity.this, homeActivity.class);
+        startActivity(intent);
+        Toast.makeText(getApplicationContext(), "Your Order have been Confirmed", Toast.LENGTH_SHORT).show();
     }
 }
